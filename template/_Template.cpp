ac48d714f17bf57818da492d0b804f9d0c784171
Costin STROIE <cstroie@bitdefender.com>
2017-10-27 18:20:12 +0300

    C++ templates.

---
+++ template/_Template.cpp
@@ -0,0 +1,16 @@
+/**
+  Template.cpp
+
+  Copyright 2017 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of RoboMitzy.
+*/
+
+#include "Arduino.h"
+#include "_Template.h"
+
+Template::Template() {
+}
+
+void Template::init() {
+}
---
+++ template/_Template.h
@@ -0,0 +1,20 @@
+/**
+  Template.h
+
+  Copyright 2017 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This file is part of RoboMitzy.
+*/
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+#include "Arduino.h"
+
+class Template {
+  public:
+    Template();
+    void init();
+};
+
+#endif /* TEMPLATE_H */
